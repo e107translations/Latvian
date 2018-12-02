732ce0254862c09d14cfa025443c9eb5e7300f74
gsandys <36930002+gsandys@users.noreply.github.com>
2018-12-02 22:11:16 +0200

    Update image.php

--- e107_languages_Latvian/admin/help/image.php
+++ e107_languages_Latvian/admin/help/image.php
@@ -16,5 +16,5 @@
 
 if (!defined('e107_INIT')) { exit; }
 
-$text = "From here you can allow / disallow the ability for users to post images on the site, set resize method and view uploaded avatars.";
-$ns -> tablerender("Images Help", $text);
+$text = "No šejienes jūs varat atļaut / aizliegt lietotājiem iespēju ievietot attēlus vietnē, iestatīt izmēru maiņas metodi un skatīt augšupielādētos attēlus.";
+$ns -> tablerender("Attēlu palīdzība", $text);
