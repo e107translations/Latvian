5e95f187cf558e076992a28ed4126cfcd2534328
gsandys <36930002+gsandys@users.noreply.github.com>
2018-12-02 22:10:45 +0200

    Update frontpage.php

--- e107_languages_Latvian/admin/help/frontpage.php
+++ e107_languages_Latvian/admin/help/frontpage.php
@@ -9,24 +9,23 @@
 
 if (!defined('e107_INIT')) { exit; }
 
-$caption = "Front Page Help";
+$caption = "Priekšējā lapa Palīdzība";
 /* FIXME - English native speakers: what should stay and what should go? 
 	<p>
-		The list of rules are scanned in turn, until the class of the current user matches. 
-		This then determines the user's front (home) page, and also the page he sees immediately after login.
+		Noteikumu saraksts tiek skenēts pēc kārtas, līdz sasniedz pašreizējā lietotāja klasi.
+Pēc tam tas nosaka lietotāja priekšējo (mājas) lapu, kā arī lapu, kuru viņš redz tūlīt pēc pieteikšanās.
 	</p>
  */
 $text = "
 <p>
-	From this screen you can choose what to display as the front page of your site, the default is news. You can also determine whether
-	users are sent to a particular page after logging in.
+	Šajā ekrānā varat izvēlēties, ko parādīt kā vietnes sākumlapu, pēc noklusējuma ir ziņas. Jūs varat arī noteikt, vai
+Lietotāji tiek sūtīti uz konkrētu lapu pēc pieteikšanās.
 </p>
 <p>
-	The rules are searched in order, to find the first where the current user belongs to the class specified in the rule. 
-	That rule then determines the front (home) page and any specific post-login page. If no rule matches, news.php is set as the home page.
-</p>
+	Noteikumi tiek meklēti pēc kārtas, lai atrastu pirmo, kur pašreizējais lietotājs pieder noteikumam, kas norādīts klasē.
+Pēc tam šis noteikums nosaka priekšējo (mājas lapu) un jebkuru konkrētu pēc pieteikšanās lapu. Ja nav noteikumu, news.php tiek iestatīts kā sākumlapa.</p>
 <p>
-	The user is sent to the specified &quot;Post-login page&quot; (if specified) immediately following a login.
+	Lietotājs tiek nosūtīts uz norādīto &quot;Post-login page&quot; (ja norādīts) tūlīt pēc pieteikšanās.
 </p>
 ";
 $ns->tablerender($caption, $text, 'admin_help');
