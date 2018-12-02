08a7dd214739c95d2b404c529bac7ce382359dca
gsandys <36930002+gsandys@users.noreply.github.com>
2018-12-02 22:31:16 +0200

    Update upload.php

--- e107_languages_Latvian/admin/help/upload.php
+++ e107_languages_Latvian/admin/help/upload.php
@@ -20,20 +20,20 @@ if (e_QUERY) list($action,$junk) = explode('.',e_QUERY); else $action = 'list';
 switch ($action)
 {
   case 'filetypes' :
-    $text = "The file types and maximum file size which can be uploaded are set by user class. This option generates a file called ".e_UPLOAD_TEMP_DIR.e_SAVE_FILETYPES.",
-		which must be copied or moved to directory ".e_ADMIN_ABS." and renamed to ".e_READ_FILETYPES." before it takes effect.<br />
-		One definition per class is permitted.<br />
-		Note that these definitions apply site-wide, even if the public uploads feature is disabled.";
+    $text = "Failu tipus un maksimālo faila izmēru, ko var augšupielādēt, nosaka lietotāja klases. Šī opcija ģenerē fails ar nosaukumu ".e_UPLOAD_TEMP_DIR.e_SAVE_FILETYPES.",
+		kas ir jāpārkopē vai jāpārvieto uz direktoriju ".e_ADMIN_ABS." un pārdēvēta par ".e_READ_FILETYPES." pirms tā stājas spēkā.<br />
+		Viena definīcija katrai klasei ir atļauta.<br />
+		Ņemiet vērā, ka šīs definīcijas tiek piemērotas visā vietnē, pat ja publiskās augšupielādes funkcija ir atspējota.";
     break;
   case 'options' :
-    $text = "The complete public uploads system may be disabled here.<br />
-		Flatfile storage is generally appropriate (otherwise the maximum file size is hard-limited to 500kb).<br />
-		The maximum filesize overrides any definitions set in filetypes.xml.<br />
-		Public uploads may be restricted to a particular user class, but these settings also apply to other areas of the site where uploads may be permitted, such as
-		news and forms.";
+    $text = "Šeit var tikt atspējota pilnīga publiskā augšupielādes sistēma.<br />
+		Flatfile uzglabāšana parasti ir piemērota (citādi maksimālais faila lielums ir grūti ierobežots līdz 500 kb).<br />
+		Maksimālais faila lielums ignorē visas definīcijas, kas noteiktas filetypes.xml.<br />
+		Publiski augšupielādētos failus var ierobežot tikai konkrētā lietotāja klasē, taču šie iestatījumi attiecas arī uz citām vietnes vietnēm, kurās var būt atļautas augšupielādes, piemēram,
+ziņas un veidlapas.";
 	break;
   case 'view' :
   default :
-	$text = "This page lists any submitted uploads. You may choose to delete them, transfer them to downloads, or post them to news";
+	$text = "Šajā lapā ir norādīti visi iesniegtie augšupielādes faili. Varat izdzēst tos, pārsūtīt tos uz lejupielādēm vai publicēt ziņas";
 }
-$ns -> tablerender("Public Uploads Help", $text);
+$ns -> tablerender("Publiska augšupielādes palīdzība", $text);
