88bcdeaf3a904e120c0e4cec4889bc75588121c1
gsandys <36930002+gsandys@users.noreply.github.com>
2018-12-02 22:17:53 +0200

    Update mailout.php

--- e107_languages_Latvian/admin/help/mailout.php
+++ e107_languages_Latvian/admin/help/mailout.php
@@ -17,64 +17,64 @@ $action = e107::getParser()->toDB(varset($_GET['mode'],'makemail'));
   switch ($action)
   {
 	case 'justone' :
-	  $text = 'Send mail with constraints specified by an optional plugin';
+	  $text = 'Sūtīt pastu ar ierobežojumiem, kurus norādījis papildu spraudnis';
 	  break;
 	case 'debug' :
-	  $text = 'For devs only. Not used at present';
+	  $text = 'Tikai izstrādātājiem. Pašlaik netiek izmantots';
 	  break;
 	case 'saved' :
-	  $text = 'Select and use a saved email template to send a mailshot. Delete any template no longer required';
+	  $text = 'Izvēlieties un izmantojiet saglabāto e-pasta veidni, lai nosūtītu mailshot. Dzēst jebkuru veidni, kas vairs nav nepieciešama';
 	  break;
 	case 'pending' :
-		$text = 'List of mailshots released for sending, together with current status. The mail scheduler task will process these emails as it is able, taking account of
-		the earliest and latest sending dates you set';
+		$text = 'Sūtīšanai izlaisto sūtījumu saraksts kopā ar pašreizējo statusu. Pasta plānotāja uzdevums apstrādās šos e-pasta ziņojumus, ņemot vērā to
+agrākos un jaunākos nosūtīšanas datumus';
 		break;
 	case 'held' :
-		$text = 'List of emails which have been prepared for sending, but not yet released';
+		$text = 'To e-pasta ziņojumu saraksts, kas sagatavoti nosūtīšanai, bet vēl nav izlaisti';
 		break;
 	case 'sent' :
-	  $text = 'List of completed mailshots. Allows you to see the sending results.<br />';
+	  $text = 'Aizpildīto mašīntulkojumu saraksts. Ļauj redzēt sūtīšanas rezultātus.<br />';
 	  break;
 	case 'savedmail' :
 	case 'makemail' :
 	case 'main' :
-	  $text = 'Create an email, give it a meaningful title, and select the list of recipients. You can save everything as a template for later, or send immediately.<br />';
-	  $text .= 'Email addresses may be contributed by plugins (such as newsletter), and duplicates are removed when the mail is sent<br />';
-	  $text .= 'Any attachment is selected from the list of valid downloads.<br />';
-	  $text .= 'Mail may be sent as plain text (most universal, and least at risk of being classed as spam), or as HTML (in which case a plain text alternative is automatically generated). The theme style
-				may optionally be added to the email. Alternatively a predefined template can be selected.';
+	  $text = 'Izveidojiet e-pastu, piešķiriet tam nozīmīgu nosaukumu un atlasiet adresātu sarakstu. Visu varat saglabāt kā veidni vēlāk vai nosūtīt uzreiz.<br />';
+	  $text .= 'E-pasta adreses var pievienot plugins (piemēram, biļetenu), un dublikāti tiek noņemti, kad pasts tiek nosūtīts<br />';
+	  $text .= 'Jebkurš pielikums ir atlasīts no derīgu lejupielāžu saraksta.<br />';
+	  $text .= 'E-pastu var nosūtīt kā vienkāršu tekstu (visizplatītākais un viszemākais drauds tikt klasificētam kā surogātpasts) vai kā HTML (tādā gadījumā tiek izveidota vienkārša teksta alternatīva). Tēmas stils
+var pēc izvēles pievienot e-pastu. Alternatīvi var izvēlēties iepriekš izveidotu veidni.';
 	  break;
 	case 'recipients' :
-		$text = 'Shows all recipients or potential recipients of an email, together with current status';
+		$text = 'Parāda visus e-pasta adresātus vai potenciālos adresātus kopā ar pašreizējo statusu';
 		break;
 	case 'prefs' :
-	  $text = '<b>Configure mailshot options.</b><br />
-	  A test email is sent using the current method and settings. If you are having problems with emails bouncing, try sending a test email to: <i>check-auth@verifier.port25.com</i> to ensure your server MX records are correct. Of course, be sure your site email address is correct before doing so.<br /><br />';
-	  $text .= '<b>Emailing Method</b><br />
-	  Use SMTP to send mail if possible. The settings will depend on your host\'s mail server.<br /><br />';
-	  $text .= '<b>Default email format</b><br />
-	  Emails may be sent either in plain text only, or in HTML format. The latter generally gives a better appearance, but is more prone to being filtered by various
-	  security measures. If you select HTML, a separate plain text part is added.<br /><br />';
-	  $text .= '<b>Bulk mail controls</b><br />
-	  The values you set here will depend on your host, and on the number of emails you send; it may be possible to set all values to zero so that the
-	  mail queue is emptied virtually instantly. Typically it is best to send less than 500 emails per hour.<br /><br />';
-	  $text .= '<b>Bounced Emails</b><br />
-	  You can specify an email address to receive the return response when an email is undeliverable. If you have control over your server, you can specify the
-	  separate scheduler-driven auto-processing script; this receives bounce messages as they arrive, and updates status instantly. Otherwise you can specify a separate email account,
-	  which can be checked either periodically (using the scheduler), or manually via the user options menu. Normally this will be a standard
-	  POP3 account; use the TLS-related options only if specifically required by your host<br /><br />';
-	  $text .= '<b>Email Address Sources</b><br />
-	  If you have additional mail-related plugins, you can select which of them may contribute email addresses to the list.<br /><br />';
+	  $text = '<b>Konfigurēt mailshot opcijas.</b><br />
+	  Testa e-pasts tiek nosūtīts, izmantojot pašreizējo metodi un iestatījumus. Ja jums rodas problēmas ar e-pasta ziņojumu strauju attīstību, mēģiniet nosūtīt testa e-pastu uz: <i> check-auth@verifier.port25.com </ i>, lai nodrošinātu, ka jūsu servera MX ieraksti ir pareizi. Protams, pārliecinieties, ka jūsu vietnes e-pasta adrese ir pareiza, pirms to izdarīt.<br /><br />';
+	  $text .= '<b>E-pasta nosūtīšanas metode</b><br />
+	  Izmantojiet SMTP, lai, ja iespējams, nosūtītu pastu. Iestatījumi būs atkarīgi no jūsu saimniekdatora pasta servera.<br /><br />';
+	  $text .= '<b>E-pasta noklusējuma formāts</b><br />
+	  E-pasta ziņojumus var nosūtīt tikai vienkāršā tekstā vai HTML formātā. Pēdējais parasti dod labāku izskatu, bet ir vairāk tendētas uz dažādu filtru
+drošības pasākumi. Ja izvēlaties HTML, tiek pievienota atsevišķa vienkārša teksta daļa.<br /><br />';
+	  $text .= '<b>Lielapjoma pasta kontrole</b><br />
+	  Šeit iestatītās vērtības būs atkarīgas no jūsu saimniekdatora un nosūtīto e-pasta ziņojumu skaita; var būt iespējams iestatīt visas vērtības uz nulli tā, lai
+pasta rinda tiek iztukšota gandrīz uzreiz. Parasti vislabāk ir nosūtīt mazāk nekā 500 e-pastu stundā.<br /><br />';
+	  $text .= '<b>Atkārtoti pa e-pastu</b><br />
+	  Jūs varat norādīt e-pasta adresi, lai saņemtu atgriešanas atbildi, ja e-pasts nav piegādājams. Ja jums ir kontrole pār savu serveri, varat norādīt
+atsevišķs plānotāja vadāms automātiskās apstrādes skripts; tā saņem atlēciņas ziņojumus, kad tie ierodas, un atjaunina statusu uzreiz. Pretējā gadījumā jūs varat norādīt atsevišķu e-pasta kontu,
+kuru var pārbaudīt periodiski (izmantojot plānotāju) vai manuāli, izmantojot lietotāja opciju izvēlni. Parasti tas būs standarts
+POP3 konts; izmantojiet ar TLS saistītās opcijas tikai tad, ja to īpaši prasījis jūsu resursdators<br /><br />';
+	  $text .= '<b>E-pasta adreses avoti</b><br />
+	  Ja jums ir papildu ar pastu saistīti spraudņi, varat izvēlēties, kuri no tiem var iekļaut sarakstā e-pasta adreses.<br /><br />';
 	  $text .= '<b>Logging</b><br />
-	  The logging option creates a text file in the system log directory. This must be deleted periodically. The \'logging
-	  only\' options allow you to see exactly who would receive emails if actually sent. The \'with errors\' option fails every
+	  The logging opcija izveido teksta failu sistēmas žurnāla direktorijā. Tas periodiski jāizdzēš. The \'logging
+	  only\' Iespējas ļauj precīzi redzēt, kas saņems e-pasta ziņojumus, ja tos faktiski nosūtīs. The \'with errors\' option fails every
 	  7th email, primarily for testing';
 	  break;
 	 case 'maint' :
-		$text = 'Maintenance functions for the mail database';
+		$text = 'Pasta datu bāzes uzturēšanas funkcijas';
 		break;
 	default :
-	  $text = 'Undocumented option';
+	  $text = 'Bezformēta iespēja';
   }
 
 $ns->tablerender('Mail Help', $text);
