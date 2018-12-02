21490a392a1e172260bd70b46c725963affd9a9d
gsandys <36930002+gsandys@users.noreply.github.com>
2018-12-02 22:28:49 +0200

    Update ugflag.php

--- e107_languages_Latvian/admin/help/ugflag.php
+++ e107_languages_Latvian/admin/help/ugflag.php
@@ -16,6 +16,6 @@
 
 if (!defined('e107_INIT')) { exit; }
 
-$text = "If you are upgrading e107 or just need your site to be offline for a while just tick the maintenance box and your visitors will be redirected to a page explaining the site is down for repair. After you've finished un-tick the box to return site to normal.";
+$text = "Ja jūs jaunināt e107 vai vienkārši nepieciešams, lai jūsu vietne kādu laiku būtu bezsaistē, vienkārši atzīmējiet uzturēšanas rūtiņu un apmeklētāji tiks novirzīti uz lapu, kurā paskaidrots, ka vietne ir salabota. Kad esat pabeidzis, atzīmējiet rūtiņu, lai atgrieztu vietni normālā stāvoklī.";
 
-$ns -> tablerender("Maintenance", $text);
+$ns -> tablerender("Apkope", $text);
