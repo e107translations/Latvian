4fc5174088972c7d7ce840656dce377245c491c1
gsandys <36930002+gsandys@users.noreply.github.com>
2018-12-02 21:44:43 +0200

    Update admin_log.php

--- e107_languages_Latvian/admin/help/admin_log.php
+++ e107_languages_Latvian/admin/help/admin_log.php
@@ -22,9 +22,9 @@ if (e_QUERY) list($action,$junk) = explode('.',e_QUERY); else $action = 'list';
 function common_filters()
 {
   $ret = "<b>Data filters</b><br />
-  You can specify various filters which restrict the amount of data to view. These are retained in a cookie until you log off.<br />
-  The start and end date/time filters have to be enabled through their respective checkboxes.<br />
-  Other filters are active when there is a value in the box.<br />
+  Jūs varat norādīt dažādus filtrus, kas ierobežo datu apjomu, kas jāpārskata. Tie tiek saglabāti sīkdatnē, līdz jūs pametat.<br />
+  Tviņam jāiespējo to sākuma un beigu datuma / laika filtri, izmantojot to attiecīgās izvēles rūtiņas.<br />
+  Citi filtri ir aktīvi, ja lodziņā ir vērtība.<br />
   ";
   return $ret;
 }
@@ -33,21 +33,21 @@ function common_filters()
 switch ($action)
 {
 case 'auditlog' :
-  $text = "This page displays that user activity which you have chosen to log.<br /><br />";
+  $text = "Šī lapa parāda lietotāja aktivitāti, kuru esat izvēlējies reģistrēt.<br /><br />";
   $text .= common_filters();
   break;
 case 'config' :
-  $text = "This page configures various options for the system logs.<br /><br />
-  <b>Common Settings</b><br />
-  Sets the number of lines on the log display.<br /><br />
-  <b>Admin Log</b><br />
-  This log retains events until specifically deleted; use this option to delete old events.<br /><br />
-  <b>User Audit Log</b><br />
-  This log tracks user activity. For registered users, only those in the specified class are tracked - you can either make this 'Members' to track 
-  everyone, or create a specific user class for logging. You then determine the types of activity which you wish to log.<br />
+  $text = "Šajā lapā konfigurē dažādas sistēmas žurnālu opcijas.<br /><br />
+  <b>Kopējie iestatījumi</b><br />
+  Iestata rindu skaitu žurnāla kurā attēlot.<br /><br />
+  <b>Admina Žurnāls</b><br />
+  Šis logs saglabā notikumus, kamēr tie nav īpaši izdzēsti; izmantojiet šo opciju, lai izdzēstu vecos notikumus.<br /><br />
+  <b>Lietotāju revīzijas žurnāls</b><br />
+  Šis žurnāls izseko lietotāju aktivitāti. Reģistrētiem lietotājiem tiek izsekoti tikai tie, kas atrodas norādītajā klasē - varat to izdarīt 'Lietotāji' izsekot
+   ikvienam vai izveidojiet īpašu lietotāju grupu mežizstrādei. Tad jūs varat noteikt darbības veidu, kuru vēlaties pieteikties.<br />
   Registration events may be tracked separately.<br /><br />
-  <b>Rolling Log</b><br />
-  This log is used to track abnormal events, assist with debugging etc. It can be disabled. Events are automatically removed after the set number of days.
+  <b>Ritošā žurnāla</b><br />
+  Šo žurnālu izmanto, lai izsekotu neparedzētus notikumus, palīdzētu ar atkļūdošanu utt. To var deaktivizēt. Notikumi automātiski tiek noņemti pēc noteiktā dienu skaita.
   ";
   break;
 case 'rolllog' :
@@ -55,21 +55,21 @@ case 'rolllog' :
   $text .= common_filters();
   break;
 case 'downlog' :
-  $text = "This page displays user downloads.<br /><br />";
+  $text = "Šajā lapā ir redzamas lietotāju lejupielādes.<br /><br />";
   $text .= common_filters();
   break;
 case 'comments' :
-  $text = "This page displays user comments, with options to select by user ID, type and date. Unwanted comments can be deleted.";
+  $text = "Šajā lapā tiek parādīti lietotāju komentāri, kuru opcijas var atlasīt pēc lietotāja ID, tipa un datuma. Nevēlamus komentārus var dzēst.";
   break;
 case 'detailed' :
-  $text = "The main system logs record time to a high degree of precision (if the underlying server supports it), and this page allows you to inspect entries
-	occuring within a relatively small time window. Entries from the admin log, audit log and rolling log are merged, so that you can see the precise 
-	relationships between events.";
+  $text = "Galvenie sistēmas žurnāli ieraksta laiku ar augstu precizitāti (ja to atbalsta serveris to atbalsta), un šī lapa ļauj jums pārbaudīt ierakstus
+	kas notiek relatīvi nelielā laika periodā. Ieraksti no administrēšanas žurnāla, revīzijas žurnāla un ritošā žurnāla ir apvienoti, lai jūs varētu redzēt precīzu 
+	attiecības starp notikumiem.";
   break;
 case 'adminlog' :
 default :
-  $text = "This page displays administrator activity.<br /><br />
-  (At present, logging is still being added to the code, so the list is not complete.)<br /><br />";
+  $text = "Šajā lapā ir redzama administratora aktivitāte.<br /><br />
+  (Šobrīd kodam joprojām tiek pievienots žurnāls, tāpēc saraksts nav pilnīgs.)<br /><br />";
   $text .= common_filters();
 }
 $ns -> tablerender($caption, $text);
